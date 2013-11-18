06a4dfd972ac537d1050ca84f02021429c206ed9
ajnaidas <ajnaidas@gmail.com>
2013-11-19 01:03:33 +0800

    initial push

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="src/less/style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="sidebar col-lg-3 col-md-2 col-sm-6 well"><h1>Left Side</h1></div>
+                <h1 class="glyphicon-pencil"></h1>
+                <div class="content col-lg-9 col-md-10 col-sm-2 well">
+                    <h1>Ultrices natoque massa mattis pulvinar integer, augue, scelerisque, elit elit mattis lacus, sed in cum facilisis adipiscing lundium nunc sed! In turpis cursus sagittis! Porttitor odio tristique magna, porta non? Vel odio est tincidunt pulvinar rhoncus tincidunt pid dapibus augue elementum mid! In magnis tempor, amet urna sit? Magnis porta montes? Mattis, aliquet augue, odio scelerisque, mid, duis natoque rhoncus! Dictumst placerat ac parturient sit, arcu urna? Porttitor placerat nunc amet in etiam adipiscing! Quis, elementum ultrices ut! Adipiscing cum. Penatibus odio vel ultricies! Mid hac, urna pellentesque elementum vel sit habitasse, aliquam porttitor, penatibus scelerisque! Eu magna dis mattis, magnis, mauris in cum sociis mid sociis etiam, et? Porta, etiam dictumst mattis dictumst porta porta! Ut? Integer tincidunt tincidunt.</h1>
+                </div>
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="public/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="public/js/vendor/bootstrap.min.js"></script>
+    </body>
+</html>
