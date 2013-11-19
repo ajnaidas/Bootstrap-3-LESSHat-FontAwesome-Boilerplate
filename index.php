9ff1ddf30f6a493da55ade7d48ffe20728521fa8
ajnaidas <ajnaidas@gmail.com>
2013-11-19 16:50:20 +0800

    Initial Push

--- index.php
+++ index.php
@@ -17,10 +17,14 @@
 
         <div class="container">
             <div class="row">
-                <div class="sidebar col-lg-3 col-md-2 col-sm-6 well"><h1>Left Side</h1></div>
-                <h1 class="glyphicon-pencil"></h1>
-                <div class="content col-lg-9 col-md-10 col-sm-2 well">
-                    <h1>Ultrices natoque massa mattis pulvinar integer, augue, scelerisque, elit elit mattis lacus, sed in cum facilisis adipiscing lundium nunc sed! In turpis cursus sagittis! Porttitor odio tristique magna, porta non? Vel odio est tincidunt pulvinar rhoncus tincidunt pid dapibus augue elementum mid! In magnis tempor, amet urna sit? Magnis porta montes? Mattis, aliquet augue, odio scelerisque, mid, duis natoque rhoncus! Dictumst placerat ac parturient sit, arcu urna? Porttitor placerat nunc amet in etiam adipiscing! Quis, elementum ultrices ut! Adipiscing cum. Penatibus odio vel ultricies! Mid hac, urna pellentesque elementum vel sit habitasse, aliquam porttitor, penatibus scelerisque! Eu magna dis mattis, magnis, mauris in cum sociis mid sociis etiam, et? Porta, etiam dictumst mattis dictumst porta porta! Ut? Integer tincidunt tincidunt.</h1>
+                <div class="sidebar col-lg-6 col-md-2 col-sm-6 col-xs-12 well">
+                    <h1>AJ Naidas</h1>
+                    <p>http://www.pixquare.com</p>
+                    <p>http://www.raptorsdojo.com</p>
+                </div>
+                <div class="content col-lg-6 col-md-10 col-sm-6 col-xs-12 well">
+                    <h1 class="glyphicon-pencil">Kickstart!</h1>
+                    <p>Ultrices natoque massa mattis pulvinar integer, augue, scelerisque, elit elit mattis lacus, sed in cum facilisis adipiscing lundium nunc sed! In turpis cursus sagittis! Porttitor odio tristique magna, porta non? Vel odio est tincidunt pulvinar rhoncus tincidunt pid dapibus augue elementum mid! In magnis tempor, amet urna sit? Magnis porta montes? Mattis, aliquet augue, odio scelerisque, mid, duis natoque rhoncus! Dictumst placerat ac parturient sit, arcu urna? Porttitor placerat nunc amet in etiam adipiscing! Quis, elementum ultrices ut! Adipiscing cum. Penatibus odio vel ultricies! Mid hac, urna pellentesque elementum vel sit habitasse, aliquam porttitor, penatibus scelerisque! Eu magna dis mattis, magnis, mauris in cum sociis mid sociis etiam, et? Porta, etiam dictumst mattis dictumst porta porta! Ut? Integer tincidunt tincidunt.</p>
                 </div>
             </div>
         </div>
