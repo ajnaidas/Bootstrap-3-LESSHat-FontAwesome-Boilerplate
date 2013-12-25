c49c9f413d28671a70708865313d232bac1be7ae
ajnaidas <ajnaidas@gmail.com>
2013-12-25 20:36:01 +0800

    initial push

--- index.php
+++ index.php
@@ -1,33 +1,79 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class=" lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class=" lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class=" lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Your site's Title</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="src/less/style.css">
+        <link rel="stylesheet" href="public/css/style.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
+        
+        <header id="main-header">
+            <h1>
+                Bootstrap 3 Project Boilerplate
+            </h1>
+        </header> <!-- #main-header -->
 
-        <div class="container">
-            <div class="row">
-                <div class="sidebar col-lg-6 col-md-2 col-sm-6 col-xs-12 well">
-                    <h1>AJ Naidas</h1>
-                    <p>http://www.pixquare.com</p>
-                    <p>http://www.raptorsdojo.com</p>
-                </div>
-                <div class="content col-lg-6 col-md-10 col-sm-6 col-xs-12 well">
-                    <h1 class="glyphicon-pencil">Bootstrap 3.0.2 with LESS</h1>
-                    <p>Ultrices natoque massa mattis pulvinar integer, augue, scelerisque, elit elit mattis lacus, sed in cum facilisis adipiscing lundium nunc sed! In turpis cursus sagittis! Porttitor odio tristique magna, porta non? Vel odio est tincidunt pulvinar rhoncus tincidunt pid dapibus augue elementum mid! In magnis tempor, amet urna sit? Magnis porta montes? Mattis, aliquet augue, odio scelerisque, mid, duis natoque rhoncus! Dictumst placerat ac parturient sit, arcu urna? Porttitor placerat nunc amet in etiam adipiscing! Quis, elementum ultrices ut! Adipiscing cum. Penatibus odio vel ultricies! Mid hac, urna pellentesque elementum vel sit habitasse, aliquam porttitor, penatibus scelerisque! Eu magna dis mattis, magnis, mauris in cum sociis mid sociis etiam, et? Porta, etiam dictumst mattis dictumst porta porta! Ut? Integer tincidunt tincidunt.</p>
-                </div>
-            </div>
-        </div>
+        <h2 class="sub">This is basically <a href="http://getbootstrap.com/">Twitter Bootstrap 3</a> packed with the following modules: </h2>
+
+        <div id="main-content" class="container">
+            <section id="packages" class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <article class="package">
+                        <h3 class="package-title">Font-Awesome 4.0.3</h3>
+                        <p class="package-description">
+                            Font Awesome gives you scalable vector icons that can instantly be customized — size, color, drop shadow, and anything that can be done with the power of CSS.
+                        </p>
+                        <p class="package-link-wrapper">
+                            Download   
+                            <a href="http://fontawesome.io/">
+                                Font-Awesome 
+                            </a>
+                            standalone at their 
+                            <a href="http://fontawesome.io/">
+                                website
+                            </a>
+                        </p>
+                    </article> <!-- .package -->
+                </div> <!-- .col -->
+
+                <div class="col-xs-12 col-sm-6">
+                    <article class="package">
+                        <h3 class="package-title">LESS Hat 2</h3>
+                        <p class="package-description">
+                            A Kick-ass LESS Mixin Library for everyone.
+                        </p>
+                        <p class="package-link-wrapper">
+                            Download    
+                            <a href="http://lesshat.com">
+                                LESS Hat
+                            </a>
+                             standalone at their 
+                            <a href="http://lesshat.com">
+                                website
+                            </a>
+                        </p>
+                    </article> <!-- .package -->
+                </div> <!-- .col -->
+            </section> <!-- #packages .row -->
+
+            <section id="reminders" class="row">
+                <div class="col-xs-12">
+                    <h4><i class="fa fa-exclamation-circle"></i>REMINDER:</h4>
+                    <p>
+                        Project files is custom arranged to my preference for a basic website. Architecture should be edited depending on 
+                        project requirements and team / personal preference.
+                    </p>
+                </div> <!-- .col -->
+            </section> <!-- #reminders .row -->
+        </div> <!-- #main-content .container -->
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="public/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
